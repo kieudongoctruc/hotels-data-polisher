33f71e945d387ea0842c173a6ca8c1b329e49188
Truc Kieu <kieudongoctruc@gmail.com>
2023-12-14 01:42:18 +0800

    feat: add and configure sidekiq

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module HotelsDataPolisher
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_job.queue_adapter = :sidekiq
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
