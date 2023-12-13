0ef8ae6548c3a51f7b826f0c00b9200cce86ebc4
Truc Kieu <kieudongoctruc@gmail.com>
2023-12-14 01:40:40 +0800

    feat: add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,24 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
 #
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+COUNTRIES = [
+  { name: 'Singapore', iso_alpha2_code: 'SG' },
+  { name: 'Japan', iso_alpha2_code: 'JP' },
+  { name: 'Malaysia', iso_alpha2_code: 'MY' },
+  { name: 'Vietnam', iso_alpha2_code: 'VN' },
+]
+COUNTRIES.each do |country|
+  Country.find_or_create_by(country)
+end
+
+AMENITIES = {
+  general: ["outdoor pool", "indoor pool", "business center", "childcare", "wifi", "dry cleaning", "breakfast", "bar", "parking", "concierge"],
+  room: ["aircon", "tv", "coffee machine", "kettle", "hair dryer", "iron", "bathtub", "minibar"]
+}
+AMENITIES.each do |amenity_type, amenities|
+  amenities.each do |name|
+    Amenity.find_or_create_by(amenity_type: amenity_type, name: name)
+  end
+end
